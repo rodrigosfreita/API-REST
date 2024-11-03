e5e2c39789adec805f0b39a3d3d6155385697aa1
Rodrigo Freitas <164232597+rodrigosfreita@users.noreply.github.com>
2024-11-03 19:12:35 -0300

    Create_FirstSpringAppAplication.java

---
+++ FirstSpringAppAplication.java
@@ -0,0 +1,13 @@
+package com.Kipper.First_Spring_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppApplication.class, args);
+	}
+
+}
